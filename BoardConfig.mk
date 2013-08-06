479933dac02e0e0d04e9cce8e9a895c3dc78dbb6
sbrissen <sbrissen@hotmail.com>
2013-08-06 07:44:18 -0400

    l900: unified fstab
    -remove recovery.fstab
    
    Change-Id: Id2f85807098069726aeb96135aa4addd839fef81

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,10 @@
 # GPS
 BOARD_GPS_SET_PRIVACY := true
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/l900/rootdir/fstab.smdk4x12
+RECOVERY_FSTAB_VERSION := 2
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := t0ltespr,t0ltecdma,l900,SPH-L900
 
