8077755a445361c7ec698979b9f0e9c681f64adb
slickrick <rsalinas1@mail.usf.edu>
2013-11-11 20:57:49 -0800

    l900: omni 4.4 updates
    - Add omni.dependencies
    - Call cdma.mk from vendor/omni
    
    Change-Id: Ie80878c9d053b3c180a3435932658c47b8e7d1ce

--- omni_l900.mk
+++ omni_l900.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Grab needed APNs
-PRODUCT_COPY_FILES := vendor/omni/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+# Inherit CDMA configuration
+$(call inherit-product, vendor/omni/config/cdma.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
